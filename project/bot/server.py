d008ddd2b1790072662bc32e52e3d9b56d5f3eab
jianghui <itjianghui@163.com>
2024-04-30 20:49:49 +0800

    增加check方法

--- project/bot/server.py
+++ project/bot/server.py
@@ -8,6 +8,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/check")
+def check():
+    return "success"
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
